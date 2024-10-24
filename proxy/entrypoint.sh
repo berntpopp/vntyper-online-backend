46f646413b3ca7cb10e7c4b5d5fea1c6626656da
Bernt Popp <bernt.popp@gmail.com>
2024-10-24 15:02:23 +0200

    Update .gitignore and docker-compose.yml files

---
+++ proxy/entrypoint.sh
@@ -0,0 +1,12 @@
+# vntyper/proxy/entrypoint.sh
+
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Replace environment variables in the Nginx template and output to the actual config
+envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+# Start Nginx
+nginx -g 'daemon off;'
