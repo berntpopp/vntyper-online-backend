f91293ee7fb17298893cb669c9702f94504c65f5
Bernt Popp <bernt.popp@gmail.com>
2024-10-31 13:40:32 +0100

    fix: configure client_max_body_size in Nginx Closes #1

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Replace environment variables in the Nginx template and output to the actual config
-envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${SERVER_NAME} ${CLIENT_MAX_BODY_SIZE}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start Nginx
 nginx -g 'daemon off;'
