9f69454a587a00ce1d260feb4ba23b7b7de74bb8
Bernt Popp <bernt.popp@gmail.com>
2024-11-04 12:44:05 +0100

    feat: update Docker configuration for Nginx with HTTP/HTTPS support and environment-specific settings

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Replace environment variables in the Nginx template and output to the actual config
-envsubst '${SERVER_NAME} ${CLIENT_MAX_BODY_SIZE}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+# Determine the environment
+if [ "$ENVIRONMENT" = "production" ]; then
+    echo "Configuring Nginx for HTTPS"
+    envsubst '${SERVER_NAME} ${CLIENT_MAX_BODY_SIZE}' < /etc/nginx/conf.d/nginx.conf.template.ssl > /etc/nginx/conf.d/default.conf
+else
+    echo "Configuring Nginx for HTTP"
+    envsubst '${SERVER_NAME} ${CLIENT_MAX_BODY_SIZE}' < /etc/nginx/conf.d/nginx.conf.template.http > /etc/nginx/conf.d/default.conf
+fi
 
 # Start Nginx
 nginx -g 'daemon off;'
